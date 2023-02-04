1cbc19b21121930358373875d3b16e016cd42c24
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-02-04 20:27:08 +0000

    :sparkles: Revert to 'Hello, World!' message (#3)

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from typing import Dict
+from typing import Mapping
 
 app: FastAPI = FastAPI()
 
 
 @app.get("/")
-def get_root() -> Dict[str, str]:
-    return {"message": "Hello, Tom!"}
+def get_root() -> Mapping[str, str]:
+    return {"message": "Hello, World!"}
